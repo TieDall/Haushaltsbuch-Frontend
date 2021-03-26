96541356f0df8251c482f61fc3343f1c45c416a0
TieDall <{ID}+{username}@users.noreply.github.com>
2021-03-26 22:39:33 +0100

    feat: kategorie

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatToolbarModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
