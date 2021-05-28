c62eeadb5561b0069a8417bdab599b5a6b40c73b
TieDall <toischro@outlook.com>
2021-05-28 14:58:43 +0200

    feat: backend reachable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from './shared/services/backend.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public isBackendReachable = false;
+
+  constructor(private readonly backendService: BackendService) { }
+
+  public ngOnInit(): void {
+    this.backendService.backendReachable.subscribe((isReachable: boolean) => {
+      this.isBackendReachable = isReachable;
+    });
+  }
 }
