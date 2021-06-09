2986f17f544c1e76557b59b8d969a5cd73bc2c65
TieDall <toischro@outlook.com>
2021-06-09 17:53:49 +0200

    feat(import_export): add export function

--- src/app/dashboard/backup-dialog/backup-dialog.component.ts
+++ src/app/dashboard/backup-dialog/backup-dialog.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppConfigService } from 'src/app/shared/services/app-config.service';
 
 @Component({
   selector: 'app-backup-dialog',
   templateUrl: './backup-dialog.component.html',
   styleUrls: ['./backup-dialog.component.scss']
 })
-export class BackupDialogComponent implements OnInit {
+export class BackupDialogComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public subscriptions = new Subscription();
 
-  ngOnInit(): void {
+  private readonly url = `${AppConfigService.appConfig.apiServer.url}Backup`;
+
+  constructor(
+    private readonly httpClient: HttpClient
+  ) { }
+
+  public export() {
+    this.subscriptions.add(
+      this.httpClient.get<Blob>(`${this.url}/Export`, {
+        observe: 'response',
+        responseType: 'blob' as 'json'
+      }).subscribe((resp: HttpResponse<Blob>) => {
+
+        let fileName = resp.headers.get('content-disposition');
+        const positionFilenameStart = fileName.indexOf('filename=');
+        fileName = fileName.substr(positionFilenameStart + 9, fileName.length);
+        const positionFilenameEnd = fileName.indexOf(';');
+        fileName = fileName.substring(0, positionFilenameEnd);
+
+        const binaryData = [];
+        binaryData.push(resp.body);
+        const data = window.URL.createObjectURL(new Blob(binaryData));
+        const link = document.createElement('a');
+        link.href = data;
+        link.download = fileName;
+        link.click();
+      })
+    );
+  }
+
+  public ngOnInit(): void {
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
