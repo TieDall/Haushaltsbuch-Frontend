1d0a701d61bac33e0744a36c2f38e650d053262b
TieDall <toischro@outlook.com>
2021-08-03 21:24:40 +0200

    feat(common): add version checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from './shared/services/backend.service';
+import { VersionService } from './shared/services/version.service';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,14 @@ import { BackendService } from './shared/services/backend.service';
 export class AppComponent implements OnInit {
 
   public isBackendReachable = false;
+  private version = version;
 
-  constructor(private readonly backendService: BackendService) { }
+  constructor(
+    private readonly backendService: BackendService,
+    private readonly versionService: VersionService
+  ) {
+    this.versionService.version = this.version;
+  }
 
   public ngOnInit(): void {
     this.backendService.backendReachable.subscribe((isReachable: boolean) => {
